34a5ce959696f0ca3e93b29e73d8eedc088e155e
Mvcampbell3 <mvcampbell3@gmail.com>
2021-07-31 14:57:51 -0400

    bootstrap and react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import './App.scss'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
 
 function App() {
-    return <div className="App"></div>
+    return (
+        <div className="App">
+            <Container className="mellow-yellow">
+                <Button type="button" variant="primary">
+                    Click
+                </Button>
+            </Container>
+        </div>
+    )
 }
 
 export default App
