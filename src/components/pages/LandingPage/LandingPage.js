a218ddf2876a2e4185445dd759f126c21ebde0b0
Mvcampbell3 <mvcampbell3@gmail.com>
2021-07-31 22:16:56 -0400

    landing page style

--- src/components/pages/LandingPage/LandingPage.js
+++ src/components/pages/LandingPage/LandingPage.js
@@ -1,17 +1,91 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
+import React, { useState } from 'react';
 import './LandingPage.scss';
-import Button from '../../common/Button';
 
 const LandingPage = () => {
+    const [displayOptions, setDisplayOptions] = useState(false);
+    const [wasToggled, setWasToggled] = useState(false);
+
+    const toggleDisplayOptions = (state) => {
+        console.log('toggle display options');
+        setDisplayOptions(state);
+        if (!wasToggled) {
+            setWasToggled(true);
+        }
+    };
+
     return (
         <div className="landing-page-background">
-            <Container fluid={'sm'}>
-                <h1 className="">This is the landing page</h1>
-                <Button type="button" variant="primary">
-                    Click
-                </Button>
-            </Container>
+            <div
+                className="mouse-enter-leave-container"
+                onMouseEnter={() => toggleDisplayOptions(true)}
+                onMouseLeave={() => toggleDisplayOptions(false)}
+            >
+                {displayOptions ? (
+                    <div className="main-title-wrapper">
+                        <div className="main-title main-title-game">
+                            Tic Tac Toe
+                        </div>
+                    </div>
+                ) : (
+                    <div className="main-title-wrapper">
+                        <div
+                            className={`main-title ${
+                                !wasToggled ? 'main-title-1' : ''
+                            }`}
+                        >
+                            Would
+                        </div>
+                        <div
+                            className={`main-title ${
+                                !wasToggled ? 'main-title-2' : ''
+                            }`}
+                        >
+                            {' '}
+                            you
+                        </div>
+                        <div
+                            className={`main-title ${
+                                !wasToggled ? 'main-title-3' : ''
+                            }`}
+                        >
+                            {' '}
+                            like
+                        </div>
+                        <div
+                            className={`main-title ${
+                                !wasToggled ? 'main-title-4' : ''
+                            }`}
+                        >
+                            {' '}
+                            to
+                        </div>
+                        <div
+                            className={`main-title ${
+                                !wasToggled ? 'main-title-5' : ''
+                            }`}
+                        >
+                            {' '}
+                            play
+                        </div>
+                        <div
+                            className={`main-title ${
+                                !wasToggled ? 'main-title-6' : ''
+                            }`}
+                        >
+                            {' '}
+                            a
+                        </div>
+                        <div
+                            className={`main-title ${
+                                !wasToggled ? 'main-title-7' : ''
+                            }`}
+                        >
+                            {' '}
+                            game?
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
