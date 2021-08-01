d18280b98e1ab2f03d8712b35d7080b6bd377208
Mvcampbell3 <mvcampbell3@gmail.com>
2021-08-01 15:27:50 -0400

    login page init

--- src/Routes.js
+++ src/Routes.js
@@ -3,16 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // Pages
 import LandingPage from './components/pages/LandingPage';
+import LoginPage from './components/pages/LoginPage';
 
 const Routes = () => {
     return (
         <Router>
             <Switch>
-                <Route
-                    exact
-                    path="/"
-                    component={LandingPage}
-                />
+                <Route exact path="/" component={LandingPage} />
+                <Route exact path="/login" component={LoginPage} />
             </Switch>
         </Router>
     );
